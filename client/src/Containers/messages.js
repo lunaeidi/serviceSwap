4ca2dff1b1127836c5b1163feb79eb503707f861
Nicole Eidi <nicoleeidi1@gmail.com>
2018-10-09 09:39:52 -0700

    messages storage

--- client/src/Containers/messages.js
+++ client/src/Containers/messages.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 class Messages extends Component{
+  state={
+    messages: []
+  }
   componentDidMount(){
     fetch('http://localhost:2000/api/messages')
     .then(res=>res.json())
-    .then(json=>console.log(json))
+    .then(json=>console.log(json)
+    let messages=[]
+    json.forEach((message)=>{
+      let obj={}
+      obj[message['sender']]= message['content']
+      messages.push(obj)
+    })
+  )
+  this.setState({messages:messages})
   }
   render() {
 
